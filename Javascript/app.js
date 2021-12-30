9bcbb46f5a1e2589758c37a31eaecff9f2de9b68
Eyup Ucmaz <eyupucmaz@gmail.com>
2021-12-30 13:57:52 +0300

    workin

--- Javascript/app.js
+++ Javascript/app.js
@@ -18,18 +18,76 @@
 // 	console.log("There is no name");
 // }
 
-let boolean;
+// let boolean;
 
-const isBooleanTrue = boolean ? true : false;
+// const isBooleanTrue = boolean ? true : false;
 
-console.log(isBooleanTrue);
+// console.log(isBooleanTrue);
 
-console.log(boolean);
+// console.log(boolean);
 
-var money;
-    var canBuy = 
-        (money < 17) ? "Satın alamazsın..":
-        (money > 30) ? "Satın alabilirsin..":
-        "Para miktarını girmen gerekmektedir..";
+// var money;
+//     var canBuy =
+//         (money < 17) ? "Satın alamazsın..":
+//         (money > 30) ? "Satın alabilirsin..":
+//         "Para miktarını girmen gerekmektedir..";
 
-    console.log(canBuy) 
+//     console.log(canBuy)
+
+// let user = {
+// 	userName: "Eyup",
+// 	isActive: true,
+// };
+// console.log(user);
+// localStorage.setItem("userInfo", JSON.stringify(user));
+
+// let userInfo = localStorage.getItem("userInfo");
+// console.log(JSON.parse(userInfo));
+
+/*
+<li class="list-group-item d-flex align-items-center">
+    <span class="me-3 badge bg-primary rounded-pill">1</span>
+    ${VALUE}
+</li>
+*/
+
+var localDB = window.localStorage;
+
+let newTasks = {
+	tasks: [],
+};
+
+let tasks = localDB.getItem("tasks")
+	? JSON.parse(localDB.getItem("tasks"))
+	: localDB.setItem("tasks", JSON.stringify(newTasks));
+
+const userForm = document.querySelector(".userForm");
+const taskList = document.querySelector(".list-group");
+
+userForm.addEventListener("submit", getValue);
+
+window.addEventListener("load", () => {
+	if (localDB.getItem("tasks")) {
+		tasks.tasks.forEach((element) => {
+			addListItem(element);
+		});
+	}
+});
+
+function getValue(event) {
+	event.preventDefault();
+	const taskInput = document.querySelector("#task").value;
+	if (taskInput.length > 0) {
+		addListItem(taskInput);
+		tasks.tasks.push(taskInput);
+		localDB.setItem("tasks", JSON.stringify(tasks));
+	}
+	taskInput.value = "";
+}
+
+function addListItem(task) {
+	let liDOM = document.createElement("li");
+	liDOM.innerHTML = `${task}`;
+	liDOM.classList.add("list-group-item", "d-flex", "align-items-center");
+	taskList.append(liDOM);
+}
---
+++ Javascript/array.js
@@ -0,0 +1,33 @@
+let items = [1,2,3,4,5,6];
+
+let femaleUsers = ['Marie',"Camila","Rachel"];
+let maleUsers = ['Kevin','David','Philipe'];
+
+console.log(items);
+console.log(femaleUsers);
+console.log(maleUsers);
+
+// Add to the end push()
+items.push(femaleUsers);
+console.log(items);
+// Add to the start unshift()
+items.unshift(maleUsers);
+console.log(items);
+// Remove from start shift()
+items.shift();
+console.log(items)
+// Remove from end pop()
+items.pop();
+console.log(items);
+
+// Getting a 3 elements of after specific elements splice(<element index>,<how many elements(includes element)>)
+let newItems = items.splice(2,3);
+console.log(newItems);
+
+// To find index of an element in array indexof()
+items.indexOf(2);
+console.log(items.indexOf(2));
+
+// In js it will be exact same array when you copy an array like that let copy = anarray;
+// 
+items.slice
