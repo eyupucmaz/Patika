03e2850255cbf3c2d5c88cfc73dba7e08e863aa4
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-01-06 19:18:44 +0300

    create app for state lecture

---
+++ React/states/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+	const [name, setName] = useState("Eyup");
+	const [age, setAge] = useState(25);
+	const [friends, setFriends] = useState(["Ahmet", "Mahmut", "Ayse"]);
+	const [address, setAddress] = useState({ title: "Istanbul", zip: 34090 });
+
+	return (
+		<div className="App">
+			<h1>Name: {name}</h1>
+			<h1>Age: {age}</h1>
+			<button onClick={() => setName("Ahmet")}>Set Name</button>
+			<button onClick={() => setAge(23)}>Set Age</button>
+
+			<hr />
+			<h2>Friends</h2>
+			{friends.map((friend, index) => (
+				<div key={index}>{friend}</div>
+			))}
+			<br />
+			<button onClick={() => setFriends([...friends, "Fatma"])}>Add Age</button>
+
+			<hr />
+			<br />
+			<h2>Address</h2>
+			<div>
+				{address.title} {address.zip}
+			</div>
+			<br />
+			<button onClick={() => setAddress({ ...address,title: "ankara" })}>
+				Set Adress
+			</button>
+		</div>
+	);
+}
+
+export default App;
