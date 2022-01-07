8ee1ff59d30fc83451de07fd800ac55e40e1e22a
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-01-07 18:53:16 +0300

    End of states chapter

--- React/states/src/components/InputExample.js
+++ React/states/src/components/InputExample.js
@@ -1,14 +1,22 @@
 import { useState } from "react";
 
 export default function InputExample() {
-	const [name, setName] = useState("");
+	// const [name, setName] = useState("");
+	const [form, setForm] = useState({ name: "", surname: "" });
+
+   const onChangeInput = (event)=>{
+      // console.log(event.target.name)
+      setForm({...form, [event.target.name]:event.target.value});
+   }
 
 	return (
 		<div>
 			Please type a name <br />
-			<input value={name} onChange={(event)=>setName(event.target.value)}/>
-         <br />
-         {name}
+			<input name="name" value={form.name} onChange={onChangeInput} />
+			<br />
+			<input name="surname" value={form.surname} onChange={onChangeInput} />
+			<br />
+			{form.name} {form.surname}
 		</div>
 	);
 }
