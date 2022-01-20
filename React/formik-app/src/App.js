440a274f07dc99f5606e54fd62c9a5f32f88dc4b
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-01-20 22:34:57 +0300

    Basics of formik and yup lib

--- React/formik-app/src/App.js
+++ React/formik-app/src/App.js
@@ -1,7 +1,10 @@
+import Signup from "./components/Signup";
+import "./App.css";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<Signup />
 		</div>
 	);
 }
---
+++ React/formik-app/src/components/Signup.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+export default function Signup() {
+	const formik = useFormik({
+		initialValues: {
+			firstName: "",
+			lastName: "",
+			email: "",
+		},
+		validationSchema: Yup.object({
+			firstName: Yup.string()
+				.max(15, "Must be 15 Characters or less")
+				.required("Required"),
+			lastName: Yup.string()
+				.max(20, "Must be 15 Characters or less")
+				.required("Required"),
+			email: Yup.string().required("Required").email("Invalid email"),
+		}),
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
+	return (
+		<div>
+			<form onSubmit={formik.handleSubmit}>
+				<div className="input-container">
+					<input
+						placeholder="Firstname"
+						type="text"
+						id="firsName"
+						name="firstName"
+						onChange={formik.handleChange}
+						value={formik.values.firstName}
+						onBlur={formik.handleBlur}
+					/>
+					{formik.touched.firstName && formik.errors.firstName ? (
+						<p>*{formik.errors.firstName}</p>
+					) : null}
+				</div>
+				<div className="input-container">
+					<input
+						placeholder="Lastname"
+						type="text"
+						id="lastName"
+						name="lastName"
+						onChange={formik.handleChange}
+						value={formik.values.lastName}
+						onBlur={formik.handleBlur}
+					/>
+					{formik.touched.lastName && formik.errors.lastName ? (
+						<p>*{formik.errors.lastName}</p>
+					) : null}
+				</div>
+				<div className="input-container">
+					<input
+						placeholder="Email"
+						type="text"
+						id="email"
+						name="email"
+						onChange={formik.handleChange}
+						value={formik.values.email}
+						onBlur={formik.handleBlur}
+					/>
+					{formik.touched.email && formik.errors.email ? (
+						<p>*{formik.errors.email}</p>
+					) : null}
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+}
